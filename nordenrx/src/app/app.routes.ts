e3b759d67588a504aeb85d9a2c8366241b35d487
DTE <w.figo.1991@gmail.com>
2018-12-13 21:40:50 -0500

    rutas

---
+++ nordenrx/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import {RouterModule, Routes} from '@angular/router';
+import {ConsultasComponent, ContactosComponent, NosotrosComponent} from './components/rutas';
+
+const app_routes: Routes = [
+    {path: '', component: NosotrosComponent},
+    {path: 'consultas', component: ConsultasComponent},
+    {path: 'contactos', component: ContactosComponent},
+    {path: '**', pathMatch: 'full', redirectTo: '' }
+];
+export const app_routing = RouterModule.forRoot(app_routes, {useHash: true});
---
+++ nordenrx/src/app/components/rutas.ts
@@ -0,0 +1,3 @@
+export { NosotrosComponent }from './nosotros/nosotros.component';
+export { ConsultasComponent }from './consultas/consultas.component';
+export { ContactosComponent }from './contactos/contactos.component';
