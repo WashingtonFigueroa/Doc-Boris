d07b0c3e968c32c20c22c53e7c0c00a007a3f18b
DTE <w.figo.1991@gmail.com>
2019-02-14 17:11:36 -0500

    scanora

--- local/copy.bat
+++ local/copy.bat
@@ -1,5 +1,5 @@
 @echo off
              :SCRIPT2
-             For %%F In ("C:\Users\HP User\Desktop\radiografia\*.*") Do If Not Exist "C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\public\radiografias\%%~nxF" Copy "%%F" "C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\public\radiografias\%%~nxF"
-             For %%F In ("C:\Users\HP User\Desktop\tomografia\*.*") Do If Not Exist "C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\public\tomografias\%%~nxF" Copy "%%F" "C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\public\tomografias\%%~nxF"
+             For %%F In ("C:\Users\pc\Desktop\radiografia\*.*") Do If Not Exist "C:\xampp\htdocs\DTE\Doc-Boris\local\public\radiografias\%%~nxF" Copy "%%F" "C:\xampp\htdocs\DTE\Doc-Boris\local\public\radiografias\%%~nxF"
+             For %%F In ("C:\Users\pc\Desktop\Tomograias\*.*") Do If Not Exist "C:\xampp\htdocs\DTE\Doc-Boris\local\public\tomografias\%%~nxF" Copy "%%F" "C:\xampp\htdocs\DTE\Doc-Boris\local\public\tomografias\%%~nxF"
              goto :SCRIPT2
