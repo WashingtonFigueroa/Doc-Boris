68b16a8671157793a89e4f620ec431e5c47a152f
DTE <w.figo.1991@gmail.com>
2019-03-19 12:06:39 -0500

    img

--- local/comprimir-archivo.bat
+++ local/comprimir-archivo.bat
@@ -1,14 +1,14 @@
 @echo off
 set carpeta=%1
 set radiografia=%2
-set carpeta_radiografia="C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\storage\app\warehouse\%1"
-set archivo="C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\storage\app\radiografias\%2"
-set instalador="C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\storage\app\instalador"
+set carpeta_radiografia="C:\xampp\htdocs\DTE\Doc-Boris\local\storage\app\warehouse\%1"
+set archivo="C:\xampp\htdocs\DTE\Doc-Boris\local\storage\app\radiografias\%2"
+set instalador="C:\xampp\htdocs\DTE\Doc-Boris\local\storage\app\instalador"
 
-set autorun="C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\storage\app\instalador\Autorun.inf"
-set dll="C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\storage\app\instalador\ImageOps.dll"
-set scanora="C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\storage\app\instalador\ScanoraLite.exe"
-set mld="C:\Users\HP User\Documents\trabajos\washington\Doc-Boris\local\storage\app\instalador\Translations.mld"
+set autorun="C:\xampp\htdocs\DTE\Doc-Boris\local\storage\app\instalador\Autorun.inf"
+set dll="C:\xampp\htdocs\DTE\Doc-Boris\local\storage\app\instalador\ImageOps.dll"
+set scanora="C:\xampp\htdocs\DTE\Doc-Boris\local\storage\app\instalador\ScanoraLite.exe"
+set mld="C:\xampp\htdocs\DTE\Doc-Boris\local\storage\app\instalador\Translations.mld"
 mkdir %carpeta_radiografia%
 copy %archivo% %carpeta_radiografia%
 copy %autorun% %carpeta_radiografia%
